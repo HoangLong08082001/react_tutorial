3af5ebb94638e6f5ac630732ae8f4bc197cbebd6
HoangLong08082001 <hahoanglong08082001@gmail.com>
2024-08-25 10:35:14 +0700

    second commit

--- lession-1/demo/src/App.js
+++ lession-1/demo/src/App.js
@@ -1,20 +1,42 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  function mouseClick() {
+    alert(`username: ${username}, password: ${password}`);
+  }
   return (
     <div className="App">
       <div className="form">
         <div className="left">
           <p className="title">TRANSPOTER MANAGEMENT</p>
           <div className="form-input">
-            <input type="text" placeholder="Enter username" name="" id="" />
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Enter username"
+              name=""
+              id=""
+            />
           </div>
           <div className="form-input">
-            <input type="text" placeholder="Enter password" name="" id="" />
+            <input
+              type="text"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Enter password"
+              name=""
+              id=""
+            />
             <p>Disable</p>
           </div>
           <div className="form-input-btn">
-            <button className="signin">SIGN IN</button>
+            <button className="signin" onClick={mouseClick}>
+              SIGN IN
+            </button>
             <button className="signup">SIGN UP</button>
           </div>
         </div>
